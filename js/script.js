fd707922c658b83ffe6cc575380acecb8a28f486
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-14 19:54:12 -0300

    add

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
    
     const menu = $('#menu');
+    const img = $('#img-menu');
     const nav = $('nav');
 
     menu.click(() => {
         nav.toggleClass('ativo');
     });
+
+    if (nav.attr('class') == 'ativo') {
+        img.attr('src', 'images/icon-close.svg');
+    }
     
 });
